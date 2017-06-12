86a9887c1dc85b37f54e060d39e717f6f057ac7e
Caroline Bowes <carvbowes@gmail.com>
2017-06-11 22:46:28 -0400

    character objects can now be referenced dynamically so that their properties remain accessible

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
-	//to do: attach character html representations to actual character objects i am dumb pls help 
-	var luke = new character("Luke Skywalker", "luke-skywalker", 100, 8, 5);
-
-	var obiWan = new character("Obi-Wan Kenobi", "obi-wan", 120, 8, 15);
-
-	var darthMaul = new character("Darth Maul", "darth-maul", 200, 8, 25);
-
-	var darthSidious = new character("Darth Sidious", "darth-sidious", 150, 8, 20);
-
-	var characters = [luke, obiWan, darthMaul, darthSidious];
 	var selections = 0; //to check for win condition
+	var attacks = 0;	//to determine player AP
+	var baseAP = 8;
 	var opponentSelect = false; //keeps track of status of game (character select, opponent select, etc)
 	var defender;
 	var player;
-	var playerAP = 8;
 
-	function character(name, id, hp, ap, counterAP) {
+	//to do: attach character html representations to actual character objects i am dumb pls help 
+	var luke = new character("Luke Skywalker", "", 100, baseAP, 5);
+
+	var obiWan = new character("Obi-Wan Kenobi", "", 120, baseAP, 15);
+
+	var darthMaul = new character("Darth Maul", "", 200, baseAP, 25);
+
+	var darthSidious = new character("Darth Sidious", "", 150, baseAP, 20);
+
+	var characters = [luke, obiWan, darthMaul, darthSidious];
+	
+
+	function character(name, img, hp, ap, counterAP) {
 		this.name = name;
-		this.id = id;
+		this.img = img;
 		this.HP = hp;
 		this.AP = ap;
 		this.counterAP = counterAP;
@@ -25,22 +28,47 @@ $(document).ready(function() {
 	}
 
 	function placeCharacters() {
-		for (i = 0; i < characters.length; i++) {
-			var charDiv = $("<div>");
-			charDiv.addClass("character-init")
-			charDiv.attr("id", characters[i].id);
-			charDiv.html(characters[i].name);
-			charDiv.data("data-attr", {hp: characters[i].hp, ap: characters[i].ap, counterAP: characters[i].counterAP});
-			// charDiv.attr("data-ap", characters[i].ap);
-			// charDiv.attr("data-counterAP", characters[i].counterAP);
-			console.log(charDiv);
+		var charIndices = Object.keys(characters)
+		for (i = 0; i < charIndices.length; i++) {
+			var charIndex = charIndices[i];
+			var character = characters[charIndex];		//allows us to reference characters array dynamically
+			var charDiv = $("<div class = 'character-init' data-name = '" + charIndex + "'>");
+			var charName = $("<div class = 'char-name'>");
+			var charImg = $("<img class = 'char-img'>");
+			var charHP = $("<div class = 'char-hp'>");
+
+			charName.text(character.name);
+			charImg.attr("src", character.img);
+			charHP.text(character.HP);
+			charDiv.append(charName).append(charImg).append(charHP);
 			$("#characters-initial").append(charDiv);
 		}
 	}
 
-	function transferDiv(obj, targetDiv) {
-		obj.appendTo(targetDiv);
+	function playerAttack() {
+		$("#game-text").html("You attacked " + defender.name + " for " + player.AP);
+		defender.HP -= player.AP;
+		$("#defender .char-hp").text(defender.HP);
+		player.AP += baseAP;
+		checkForWin();
 
+	}
+
+	function defenderAttack() {
+		$("#game-text").append("<p>" + defender.name + " attacked you for " + defender.counterAP + "</p>");
+		player.HP -= defender.counterAP;
+		$("#player .char-hp").text(player.HP);
+		checkForLose();
+	}
+
+	function checkForWin() {
+		if (defender.HP <= 0) {
+			
+		}
+	}
+
+	function checkForLose() {
+		return player.HP <= 0;
 	}
 
 	placeCharacters();
@@ -49,36 +77,38 @@ $(document).ready(function() {
 	
 	$("#characters-initial").on("click", ".character-init", function(event) { 
 		if (selections === 0 && !opponentSelect) {
-			// "this" is the element the user clicked on??
-			transferDiv($(this), $("#player-character"));
+			// "this" is the element the user clicked on
+			var playerKey = $(this).attr("data-name");
+			player = characters[playerKey];
+			console.log("You have selected " + player.name);
+			$(this).appendTo($("#player-character"));
 			$(this).removeClass("character-init").attr("id", "player");
 			$("#characters-initial").children(".character-init").each(function () {
 	 			// "this" is the current element in the loop
-    			transferDiv($(this), $("#opponents"));
+	 			$(this).appendTo($("#opponents"));
     			$(this).removeClass("character-init").addClass("opponent")
-    			console.log($(this).attr("class"));
 			});
 
 			opponentSelect = true;
 			selections++;
-			player = $(this);
-			console.log(opponentSelect);
-			console.log(selections);
-
 		} 
 	});
 
 	$("#opponents").on("click", ".opponent", function(event) {
 		if (opponentSelect) {
-			transferDiv($(this), $("#defender-div"));
+			var defenderKey = $(this).attr("data-name");
+			defender = characters[defenderKey];
+			console.log("Your opponent is " + defender.name);
+			$(this).appendTo($("#defender-div"));
 			$(this).removeClass("opponent").attr("id", "defender");
+
+
 			opponentSelect = false;
 			selections++;
+			$('#attack').prop('disabled', false);
 			console.log(opponentSelect);
 			console.log(selections);
-			$("#attack").prop('disabled', false);
-			defender = $(this); 
-			console.log(this);
+
 		}
 
 	});
@@ -86,7 +116,11 @@ $(document).ready(function() {
 
 
 	$("#attack").click(function (event) {
-		$("#game-text").html("You attacked " + defender.text() + " for " + player.attr("data-ap")) ;
+		console.log(player);
+		console.log(defender);
+		playerAttack();
+		defenderAttack();
+
 
 	});
 
